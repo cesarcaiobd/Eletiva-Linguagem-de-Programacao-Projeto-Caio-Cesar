af2e176798f4a661be034dda9298ad24c76beb6b
cesarcaiobd <100534850+cesarcaiobd@users.noreply.github.com>
2022-04-08 21:56:21 -0300

    Exercicio 4 Lista 2

--- Exercicios/lista de exercicios 2/Exercicio 4/resultado.php
+++ Exercicios/lista de exercicios 2/Exercicio 4/resultado.php
@@ -1,4 +1,4 @@
-!doctype html>
+<!doctype html>
 <html lang="en">
   <head>
     
@@ -8,38 +8,25 @@
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Lista 2 Exercicio 3 Resultado</title>
+    <title>Lista 2 Exercicio 4 Resultado</title>
   </head>
   <body>
-    <h1>Lista 2 Exercicio 3 Resultado</h1>
+    <h1>Lista 2 Exercicio 4 Resultado</h1>
 
-   
     <?php
-        for ($i=1; $i <=5; $i++){          
+        $soma = 0;
+
+        for ($i=1; $i <=10; $i++){          
                 $vetor[$i] = $_POST["valor$i"];
+                $soma += $vetor[$i];
+
         }
 
-        foreach($vetor as $chave => $valor){
-            $contador = 0;
-            $valorIgual = $valor;
+        $media = $soma / 10;
 
-            foreach($vetor as $v){
-                if ($valorIgual == $v){
-                    $contador++;
-                }
-            }
-
-            if ($contador > 1){
-                $vetor[$chave] = "-";
-                
-            }
-        }
-
-        foreach($vetor as $valor){
-            echo $valor;
-        }
-        
+        echo $media;
     ?>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
