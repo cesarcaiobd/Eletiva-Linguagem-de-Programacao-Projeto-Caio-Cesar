a70092e85408e6f3690583fa800c3b79aac2e40c
cesarcaiobd <100534850+cesarcaiobd@users.noreply.github.com>
2022-06-02 22:14:11 -0300

    02-06-2022
    Produtos Controller alteração

--- ProjetoPhp/src/Controller/ProdutosController.php
+++ ProjetoPhp/src/Controller/ProdutosController.php
@@ -6,26 +6,62 @@ use Aluno\ProjetoPhp\Model\DAO\ProdutosDAO;
 use Aluno\ProjetoPhp\Model\Entity\Produtos;
 
 class ProdutosController{
+
+    //private ClientesDAO $dao;
+
     public static function abrirFormularioInserir(){
         require_once "../src/View/inserir_produto.php";
     }
 
+    public static function abrirFormularioAlterar($params){
+        $dao = new ProdutosDAO();
+        $resultado = $dao->consultarPorId($params[1]);
+        require_once "../src/View/alterar_produto.php";
+    }
+
     public static function abrirListaProdutos(){
+        $dao = new ProdutosDAO();
+        $resultado = $dao->consultar();
         require_once "../src/View/listar_produtos.php";
     }
 
     public static function inserirProduto(){
         $produto = new Produtos();
-        $produto->setNome($_POST["nome"]);
-        $produto->setDescricao($_POST["descricao"]);
-        $produto->setValor($_POST["valor"]);
+        $produto->setDescricao($_POST['descricao']);
+        $produto->setValor($_POST['valor']);
+        $produto->setNome($_POST['nome']);
         $dao = new ProdutosDAO();
         if ($dao->inserir($produto)){
             $resposta = true;
-        }
-        else{
+        } else {
             $resposta = false;
         }
-        require_once "::/src/View/listar_produtos.php";
+        ProdutosController::abrirListaProdutos();
     }
+
+    public static function editarProduto($params){
+        $produto = new Produtos();
+        $produto->setDescricao($_POST['descricao']);
+        $produto->setValor($_POST['valor']);
+        $produto->setNome($_POST['nome']);
+        $produto->setId($params[1]);
+        $dao = new ProdutosDAO();
+        if ($dao->alterar($produto)){
+            $resposta = true;
+        } else {
+            $resposta = false;
+        }
+        ProdutosController::abrirListaProdutos();
+    }
+
+    public static function excluirProduto($params){
+        $dao = new ProdutosDAO();
+        if ($dao->excluir($params[1])){
+            $resposta = true;
+        } else {
+            $resposta = false;
+        }
+        ProdutosController::abrirListaProdutos();
+    }
+
 }
